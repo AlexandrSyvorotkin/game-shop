7419aef8707c97b65dc199ef68e434953ddf70f7
Alex <Alex031994@gmail.com>
2022-08-04 21:18:10 +0300

    game page component + cartBlock

--- src/components/CartBlock/CartBlock.jsx
+++ src/components/CartBlock/CartBlock.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {AiOutlineShoppingCart} from "react-icons/ai";
 import {useSelector} from "react-redux";
 import {calcTotalPrice} from "../../utils/utils";
 import CartMenu from "../CartMenu/CartMenu";
 import ItemsInCart from "../ItemsInCart/ItemsInCart";
 import './CartBlock.css'
+import {useNavigate} from "react-router-dom";
 
 
 const CartBlock = () => {
@@ -12,6 +13,12 @@ const CartBlock = () => {
     const [isCartMenuVisible, setIsCartMenuVisible] = useState(false)
     const items = useSelector(state => state.cart.itemsInCart)
     const totalPrice = calcTotalPrice(items)
+    const navigate = useNavigate()
+
+    const handleClick = useCallback(() => {
+        setIsCartMenuVisible(false)
+        navigate('/order')
+    }, [navigate])
 
 
     return (
@@ -19,7 +26,7 @@ const CartBlock = () => {
             <ItemsInCart quantity={items.length}/>
             <AiOutlineShoppingCart size={20} onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}/>
             {totalPrice > 0 ? <span className='cart-block__total-price'>{totalPrice}</span> : null}
-            { isCartMenuVisible && <CartMenu items={items} onClick={() => null}/>}
+            { isCartMenuVisible && <CartMenu items={items} onClick={handleClick}/>}
         </div>
     );
 };
--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import './GamePage.css'
+import {useSelector} from "react-redux";
+import {GameCover} from "../../components/GameCover/GameCover";
+import GameGenre from "../../components/GameGenre/GameGenre";
+import GameBuy from "../../components/GameBuy/GameBuy";
+import './GamePage.css'
 
 const GamePage = () => {
+
+    const game = useSelector(state => state.game.currentGame)
+
+    if (!game) return null
+
     return (
-        <div>
-            GamePage
+        <div className='game-page'>
+            <h1 className='game-page__title'>{game.title}</h1>
+            <div className="game-page__content">
+                <div className="game-page__left">
+                    <iframe src={game.video} width='90%' height='400px' title='Youtube Video Player' frameBorder='null'>
+                    </iframe>
+                </div>
+                <div className="game-page__right">
+                    <GameCover  image={game.image}/>
+                    <p>{game.description}</p>
+                    <p className='secondary-text'>Популярные метки этого продукта:</p>
+                    <div className='game-item__genre'>
+                        {game.genres.map(genre =>
+                            <GameGenre genre={genre} key={genre}/>
+                        )}
+                    </div>
+                    <div className="game-page__buy">
+                        <GameBuy game={game}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
