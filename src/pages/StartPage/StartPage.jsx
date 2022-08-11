e0319c0acae60e115a3b68e34b640c771c96e931
Your Name <you@example.com>
2022-08-11 07:00:35 +0300

    modified start page

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -6,6 +6,9 @@ const StartPage = ({enterShopHandler}) => {
 
     return (
         <div className='startPage'>
+            <div className='gameshop'>
+                <span>GAME SHOP</span>
+            </div>
             <StartButton onClick={enterShopHandler}>Войти</StartButton>
         </div>
     );
